85f4838bb72efe0022244a0ef37ca41ed9793572
David Su <david.d.su@gmail.com>
2017-11-07 14:33:28 -0500

    create unity project; import affdex; setup camera stream face analyzer (PlayerEmotions.cs)

---
+++ AMAI/Assets/Scripts/PlayerEmotions.cs
@@ -0,0 +1,58 @@
+﻿// Adapted from https://knowledge.affectiva.com/docs/analyze-the-camera-stream-4
+
+using Affdex;
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class PlayerEmotions : ImageResultsListener
+{
+	public float currentSmile;
+	public float currentInterocularDistance;
+	public float currentContempt;
+	public float currentValence;
+	public float currentAnger;
+	public float currentFear;
+	public float currentSadness;
+	public FeaturePoint[] featurePointsList;
+
+	public override void onFaceFound(float timestamp, int faceId)
+	{
+		Debug.Log("Found the face");
+	}
+
+	public override void onFaceLost(float timestamp, int faceId)
+	{
+		Debug.Log("Lost the face");
+	}
+
+	public override void onImageResults(Dictionary<int, Face> faces)
+	{
+		Debug.Log("Got face results");
+
+		foreach (KeyValuePair<int, Face> pair in faces)
+		{
+			int FaceId = pair.Key;  // The Face Unique Id.
+			Face face = pair.Value;    // Instance of the face class containing emotions, and facial expression values.
+
+			//Retrieve the Emotions Scores
+			face.Emotions.TryGetValue(Emotions.Contempt, out currentContempt);
+			face.Emotions.TryGetValue(Emotions.Valence, out currentValence);
+			face.Emotions.TryGetValue(Emotions.Anger, out currentAnger);
+			face.Emotions.TryGetValue(Emotions.Fear, out currentFear);
+			face.Emotions.TryGetValue(Emotions.Fear, out currentSadness);
+
+			//Retrieve the Smile Score
+			face.Expressions.TryGetValue(Expressions.Smile, out currentSmile);
+
+
+			//Retrieve the Interocular distance, the distance between two outer eye corners.
+			currentInterocularDistance = face.Measurements.interOcularDistance;
+
+
+			//Retrieve the coordinates of the facial landmarks (face feature points)
+			featurePointsList = face.FeaturePoints;
+
+		}
+	}
+}
